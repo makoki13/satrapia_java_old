ddde2139af823efbefbc4695f73f52a099189104
makoki <pablo.prieto@dulcesol.es>
2016-05-18 17:52:45 +0200

    TEst

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Comentario antes de saber java :-D
-		System.out.println("Hola java");
+		System.out.println("Hola");
 	}
 
 }
