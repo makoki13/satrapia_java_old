7f730dd92ac2bda62d14fc1cba802fbe35dc8635
makoki <pablo.makoki@gmail.com>
2016-05-18 18:13:06 +0200

    TEST

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,7 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Comentario antes de saber java :-D
-		System.out.println("Hola, pollo -----");		
+		System.out.println("Hola, pollo -----");
+		System.out.println("Que tal, pollo -----");
 		System.out.println("Adios, pollo -----");
 	}
 
