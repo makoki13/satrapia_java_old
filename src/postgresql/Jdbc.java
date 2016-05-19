389b614642ac1e5bbc3f9ffe42346bb069766532
makoki <pablo.makoki@gmail.com>
2016-05-19 17:31:43 +0200

    Mas trabajo

---
+++ src/postgresql/Jdbc.java
@@ -0,0 +1,53 @@
+package postgresql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Jdbc {
+	static public Connection conecta(String servidor,String basededatos, String usuario, String password) {
+		String cc = "jdbc:postgresql://172.16.0.72/Produccion?user=postgres&password=vimvsp";
+		try {
+			Class.forName("org.postgresql.Driver");
+			Connection conexion = DriverManager.getConnection(cc);
+			return conexion;
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+			return null;
+		}		
+	}
+	
+	static public ResultSet consulta(Connection conexion, String sql) {
+		Statement comando = null;
+		try {
+			comando = conexion.createStatement();		
+			ResultSet resultado = comando.executeQuery(sql);
+			return resultado;
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+			try {
+				comando.close();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			return null;
+		}
+	}
+	
+	static public int ejecuta(Connection conexion, String sql) {
+		Statement comando = null;
+		try {
+			comando = conexion.createStatement();		
+			int filas = comando.executeUpdate(sql);
+			return filas;
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+			return -1;
+		}		 
+				
+	}
+}
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,28 +1,34 @@
 package test;
 
 import java.sql.*;
+import postgresql.*;
 
 public class Test {
 	
-	public static void conecta() {
-		String cc = "jdbc:postgresql://172.16.0.72/Produccion?user=postgres&password=vimvsp";
+	public static void conecta() {		
+		String servidor = "172.16.0.72";
+		String baseDeDatos = "Produccion";
+		String usuario = "postgres";
+		String password = "vimvsp";
+		Connection conexion = Jdbc.conecta(servidor, baseDeDatos, usuario, password);
 		try {
-			Class.forName("org.postgresql.Driver");
-			Connection conexion = DriverManager.getConnection(cc);
-			Statement comando = conexion.createStatement();
 			String sql ="SELECT Codigo,Nombre FROM Articulos WHERE (Alta='S' or Alta IS NULL) AND Tipo IN ('P') ORDER BY Codigo LIMIT 5";
-			ResultSet resultado = comando.executeQuery(sql);
+			ResultSet resultado = Jdbc.consulta(conexion, sql);
 			while(resultado.next()) {
 				String n = resultado.getString("Codigo");
 				String a = resultado.getString("Nombre");
 				System.out.println(n + " " + a);
 			}
-			resultado.close();
-			comando.close();
-			conexion.close();
+			resultado.close();			
+						
+			sql = "UPDATE articulos_200809 SET Nombre='VALENCIANA CONSUM 10 ptes. 700gr.' WHERE Codigo='01070'";
+			Integer filas = Jdbc.ejecuta(conexion, sql);
+			System.out.println("Modificadas "+filas.toString()+" filas");
+			
+			conexion.close();			
 		} catch(Exception e) {
 			System.out.println(e.getMessage());
-		}
+		}				
 	}
 
 	public static void main(String[] args) {
