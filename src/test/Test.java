66a7005c1025a2933453f1810a05de942972bf2d
makoki <pablo.makoki@gmail.com>
2016-05-19 16:39:41 +0200

    Test jdbc

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,13 +1,33 @@
 package test;
 
+import java.sql.*;
+
 public class Test {
+	
+	public static void conecta() {
+		String cc = "jdbc:postgresql://172.16.0.72/Produccion?user=postgres&password=vimvsp";
+		try {
+			Class.forName("org.postgresql.Driver");
+			Connection conexion = DriverManager.getConnection(cc);
+			Statement comando = conexion.createStatement();
+			String sql ="SELECT Codigo,Nombre FROM Articulos WHERE (Alta='S' or Alta IS NULL) AND Tipo IN ('P') ORDER BY Codigo LIMIT 5";
+			ResultSet resultado = comando.executeQuery(sql);
+			while(resultado.next()) {
+				String n = resultado.getString("Codigo");
+				String a = resultado.getString("Nombre");
+				System.out.println(n + " " + a);
+			}
+			resultado.close();
+			comando.close();
+			conexion.close();
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Comentario antes de saber java :-D
-		System.out.println("Hola, pollo -----");
-		System.out.println("Que tal, pollo -----");
-		System.out.println("Adios, pollo -----");
+		System.out.println("Inicio -----");
+		Test.conecta();
 	}
 
 }
