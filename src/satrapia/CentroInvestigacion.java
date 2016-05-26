c087a46c0c60e7572a5a10d4b5c9e097d53a4810
makoki <pablo.makoki@gmail.com>
2016-05-26 14:40:08 +0200

    Mas curro

--- src/satrapia/CentroInvestigacion.java
+++ src/satrapia/CentroInvestigacion.java
@@ -1,5 +1,151 @@
 package satrapia;
 
 public class CentroInvestigacion {
+	private Jugador jugador;
 
+    private ArrayList<Investigacion> investigaciones;
+
+    public CentroInvestigacion(Jugador jugador)
+    {
+        this.jugador = jugador;
+        this.investigaciones = _interfaz_investigacion.getInvestigaciones(this.jugador.getID());       
+    }
+
+    public Investigacion getInvestigacion(Investigacion.TiposInvestigacion tipo)
+    {
+        int index = investigaciones.FindIndex(
+        delegate (Investigacion miInvestigacion)
+        {
+            return (miInvestigacion.getTipo() == tipo);
+        });
+
+        return investigaciones.ElementAt(index);
+    }
+
+    public bool sePuedeComprarSiguienteNivel(Investigacion.TiposInvestigacion tipo)
+    {
+        //Primero: Hay siguiente nivel;
+        Investigacion v = this.getInvestigacion(tipo);
+        int nivelActual = this.nivelActual(v);
+        int siguienteNivel = this.ultimoNivel(v);
+        if (nivelActual >= siguienteNivel) return false;
+
+        //Segundo: Tenemos los recursos necesarios
+        //Hay que determinar una manera de obtener los recursos que tenemos en Palacio -> Recursos naturales: Almacenes, Recursos artificiales y Oro: Palacio, Dinero: Banco.
+        Poblacion capital = this.jugador.dirigente.capital;
+        int celda = capital.idCelda();
+
+        List<Recurso> listaRecursos = v.costeDeNivelSiguiente();
+        foreach(Recurso r in listaRecursos)
+        {
+            Recurso.TiposRecurso t = r.tipo;
+            switch(t)
+            {
+                //En Banco
+                case DINERO:                    
+                    {
+                        Productor p = new Productor(this.jugador.getID(), celda, Productor.TiposProductor.BANCO);
+                        long dinero = p.stock(Recurso.TiposRecurso.DINERO);
+                        if (r._get_Cantidad() > dinero) return false; //Ya hemos terminado... No hay pasta.
+                        break;
+                    }
+
+                //En Almacen
+                case COMIDA:
+                case HIERRO:
+                case MADERA:
+                case PIEDRA:
+                    {
+                        Productor p = new Productor(this.jugador.getID(), celda, Productor.TiposProductor.ALMACEN);
+                        long tenemos = p.stock(t);
+                        if (r.cantidad > tenemos) return false; //Ya hemos terminado... No hay recurso... Habría que saber que recursos...
+                        break;
+                    }
+
+                //En Palacio
+                case Recurso.TiposRecurso.ORO:
+                default:
+                    {
+                        Productor p = new Productor(this.jugador.getID(), celda, Productor.TiposProductor.CASTILLO);
+                        long tenemos = p.stock(t);
+                        if (r.cantidad > tenemos) return false; //Ya hemos terminado... No hay recurso... Habría que saber que recursos...
+                        break;
+                    }
+            }
+        }
+
+        return true;
+    }
+
+    public int compraSiguienteNivelInvestigacion(Investigacion.TiposInvestigacion tipo)
+    {
+        Poblacion capital = this.jugador.dirigente.capital;
+        int celda = capital.idCelda();
+
+        Investigacion v = this.getInvestigacion(tipo);
+
+        List<Recurso> listaRecursos = v.costeDeNivelSiguiente();
+        foreach (Recurso r in listaRecursos)
+        {
+            Recurso.TiposRecurso t = r.tipo;
+            switch (t)
+            {
+                //En Banco
+                case DINERO:
+                    {
+                        Productor p = new Productor(this.jugador.getID(), celda, Productor.TiposProductor.BANCO);
+                        long dinero = p.stock(Recurso.TiposRecurso.DINERO);
+                        if (r.cantidad > dinero) return -1; //Ya hemos terminado... No hay pasta.
+                        p.saca(Recurso.TiposRecurso.DINERO, r.cantidad);
+                        break;
+                    }
+
+                //En Almacen
+                case COMIDA:
+                case HIERRO:
+                case MADERA:
+                case PIEDRA:
+                    {
+                        Productor p = new Productor(this.jugador.getID(), celda, Productor.TiposProductor.ALMACEN);
+                        long tenemos = p.stock(t);
+                        if (r.cantidad > tenemos) return -2; //Ya hemos terminado... No hay recurso... Habría que saber que recursos...
+                        p.saca(t, r.cantidad);
+                        break;
+                    }
+
+                //En Palacio
+                case ORO:
+                default:
+                    {
+                        Productor p = this.jugador.dirigente.palacio;
+                        long tenemos = p.stock(t);
+                        if (r.cantidad > tenemos) return -3; //Ya hemos terminado... No hay recurso... Habría que saber que recursos...
+                        p.saca(t, r.cantidad);
+                        break;
+                    }
+            }
+        }
+
+        //Ahora incluimos la investigación en nuestra lista de investigaciones compradas.
+        v.setInvestigada(this.jugador);
+
+        return 0;
+    }
+
+    public int nivelActual (Investigacion inv)
+    {
+        return inv.nivelActual;
+    }
+
+    public int ultimoNivel(Investigacion inv)
+    {
+        return inv.maxNivel;
+    }
+
+    public int mejora (Productor prod, Potencial pot)
+    {
+        return 0;
+    }
+
+    public List<Investigacion> getLista() { return this.investigaciones; }
 }
--- src/satrapia/Investigacion.java
+++ src/satrapia/Investigacion.java
@@ -1,7 +1,10 @@
 package satrapia;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
+import postgresql.Jdbc;
 import satrapia.Recurso.TiposRecurso;
 
 public class Investigacion {        
@@ -55,5 +58,115 @@ public class Investigacion {
         this.nivelActual++;
         _interfaz_investigacion.guardaInvestigacionJugador(this.getTipo(), this.nivelActual, j.getID(), false);
     }
+}
 
+class _interfaz_investigacion {
+	public static ArrayList<Investigacion> getInvestigaciones(long jugador)
+    {
+        int id;
+        String nombre;
+        Investigacion.TiposInvestigacion tipo;
+        int nivelActual;
+        int maxNivel;
+        int tiempo = 0;            
+        ArrayList<Investigacion> lista = new ArrayList<Investigacion>();
+        ArrayList<Recurso> listaRecursosSiguienteNivel; 
+
+        Investigacion d;            
+
+        String sql = ""+
+            "SELECT "+
+                "ID,Nombre,"+                    
+                "COALESCE((SELECT Nivel FROM Investigaciones_Usuarios WHERE ID_Investigacion=P.ID AND ID_Usuario= "+ jugador + "),0) AS Nivel,"+
+                "COALESCE((SELECT Max(Nivel) FROM Investigaciones_Niveles WHERE ID_Investigacion = P.ID),1) AS MaxNivel " +
+            "FROM Investigaciones P "+
+            "ORDER BY ID"+                
+        "";
+        ResultSet resultado = Jdbc.consulta(Mapa.conexion, sql);
+		try {
+			while(resultado.next()) {
+				id = resultado.getInt("ID");
+				nombre = resultado.getString("Nombre");
+				tipo = Investigacion.TiposInvestigacion.values()[id];
+				nivelActual = resultado.getInt("Nivel");
+				maxNivel = resultado.getInt("MaxNivel");
+
+				listaRecursosSiguienteNivel = null;
+	            if (nivelActual < maxNivel) //Si quedan niveles para investigar determinamos que se necesita
+	            {
+	                int siguienteNivel = nivelActual + 1;
+	                int idRecurso = -1;
+	                Long cantidad = (long) 0;                    
+	                Recurso r;
+
+	                sql = "SELECT COALESCE(Tiempo,0) AS Tiempo FROM Investigaciones_Niveles WHERE ID_Investigacion = " + id + " AND Nivel = " + siguienteNivel;
+	                ResultSet resultado2 = Jdbc.consulta(Mapa.conexion, sql);
+	                try {
+	        			while(resultado.next()) {
+	        				tiempo = resultado2.getInt("Tiempo");
+	        			}
+	                } catch (SQLException e) {
+	            			e.printStackTrace();
+	            	}
+	                
+	                listaRecursosSiguienteNivel = new ArrayList<Recurso>();
+	                sql = "SELECT ID_Recurso,Cantidad FROM Investigaciones_Niveles_Recursos WHERE ID_Investigacion = " + id + " AND Nivel = " + siguienteNivel;
+	                ResultSet resultado3 = Jdbc.consulta(Mapa.conexion, sql);
+	                try {
+	        			while(resultado.next()) {
+	        				idRecurso = resultado3.getInt("ID_Recurso");
+	        				cantidad = resultado3.getLong("Cantidad");
+	        				r = new Recurso(Recurso.TiposRecurso.values()[idRecurso], cantidad, -1, Recurso.TiposReceptor.INVESTIGACION);
+	                        listaRecursosSiguienteNivel.add(r);
+	        			}
+	                } catch (SQLException e) {
+	            			e.printStackTrace();
+	            	}
+	            }
+	            d = new Investigacion(Investigacion.TiposInvestigacion.values()[id], nombre, nivelActual, maxNivel, listaRecursosSiguienteNivel, tiempo);
+	            lista.add(d);
+			}
+		} catch (SQLException e) {
+		// 	TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return lista;            
+    }
+
+    public static int guardaInvestigacionJugador(Investigacion.TiposInvestigacion tipo, int nivel, long jugador, boolean pendiente)
+    {
+        String sql = "SELECT COUNT(*) AS NumRegistros FROM Investigaciones_Usuarios WHERE ID_Usuario = " + jugador + " AND ID_Investigacion = " + (int)tipo;
+        ResultSet resultado = Jdbc.consulta(Mapa.conexion, sql);
+        Long numRegistros = 0;
+        try {
+			while(resultado.next()) {
+				numRegistros = resultado.getLong(0);
+				
+			}
+        } catch (SQLException e) {
+    			e.printStackTrace();
+    	}
+        
+        if (numRegistros==0)
+            sql = "INSERT INTO Investigaciones_Usuarios (ID_Usuario,ID_Investigacion,Nivel, Pendiente) VALUES (" + jugador + "," + (int)tipo + "," + nivel + ",'N')";
+        else
+            sql = "UPDATE Investigaciones_Usuarios SET Nivel = " + nivel + " WHERE ID_Usuario = " + jugador + " AND ID_Investigacion = " + (int)tipo;
+        int filas = Mapa.conexion.ejecuta(sql);
+        if (filas == -1) return -1; //Aqui mejor lanzar una excepcion
+        return 0;
+    }
+
+    public static bool estaInvestigada(Investigacion.TiposInvestigacion tipo, int nivel, long idJugador) {
+        string sql = "SELECT COUNT(*) AS NumRegistros FROM Investigaciones_Usuarios WHERE ID_Usuario = " + idJugador + " AND ID_Investigacion = " + (int)tipo + " AND Nivel = " + nivel;
+        DataTable dt;
+        dt = Mapa.conexion.consulta(sql);
+        Int64 numRegistros = 0;
+        foreach (DataRow row in dt.Rows)
+        {
+            numRegistros = row.Field<Int64>("NumRegistros");
+        }
+
+        return (numRegistros>0);
+    }
 }
