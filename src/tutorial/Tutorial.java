a39424459b08fc976ca8c9dacc68c35b5394ebb4
makoki <pablo.makoki@gmail.com>
2016-05-30 18:07:35 +0200

    Mas curro

--- src/tutorial/Tutorial.java
+++ src/tutorial/Tutorial.java
@@ -1,10 +1,71 @@
 package tutorial;
 
-public class Tutorial {
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
 
-	}
+public class Tutorial extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	public Tutorial() {
+
+        initUI();
+    }
+	
+	private void createLayout(JComponent... arg) {
+
+        Container pane = getContentPane();
+        GroupLayout gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+
+        gl.setAutoCreateContainerGaps(true);
+
+        gl.setHorizontalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+
+        gl.setVerticalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+    }
+
+    private void initUI() {
+        
+        setTitle("Satrapía: El tutorial");
+        setSize(1024, 768);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        JButton quitButton = new JButton("Quit");
+
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.exit(0);
+            }
+        });
+
+        createLayout(quitButton);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+        
+            @Override
+            public void run() {
+                Tutorial ex = new Tutorial();
+                ex.setVisible(true);
+            }
+        });
+    }
 }
