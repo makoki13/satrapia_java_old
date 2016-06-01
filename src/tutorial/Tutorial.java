bbd4508f186276ed167f27984acf99fb7aee4284
usuario <usuario>
2016-06-01 12:41:37 +0200

    Nuevo eclipse

--- src/tutorial/Tutorial.java
+++ src/tutorial/Tutorial.java
@@ -1,14 +1,11 @@
 package tutorial;
 
-import java.awt.Container;
-import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
-import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
